8edeb7ae72ad1465d21a1d8bf462f7624a33cbb0
Stefaan Lippens <soxofaan@gmail.com>
2018-01-04 21:49:27 +0100

    Change to MIT license

--- duviz.py
+++ duviz.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
-##############################################################################
-# Copyright 2009-2016 Stefaan Lippens
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##############################################################################
 
 """
 Command line tool for visualization of the disk space usage of a directory
 and its subdirectories.
 
-Copyright: 2009-2016 Stefaan Lippens
+Copyright: 2009-2018 Stefaan Lippens
+License: MIT
 Website: http://soxofaan.github.io/duviz/
 """
 
