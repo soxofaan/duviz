c00c27297c17c53546bf2e5258fd1fc70ac2e934
Stefaan Lippens <soxofaan@gmail.com>
2019-10-20 14:29:55 +0200

    Bump version to 2.0.1 and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@
 ## [Unreleased]
 
 
+## [2.0.1] - 2019-10-20
+
+- Replace custom terminal size detection code with `shutil.get_terminal_size` (Issue #6)
+- Trying out https://deepsource.io/ for static code analysis
+- Add Travis run for Python 3.8 (instead of 3.8-dev)
+
 ## [2.0.0] - 2019-10-20
 
 - Dropped Python 2 support
--- duviz.py
+++ duviz.py
@@ -323,7 +323,7 @@ def main():
         """usage: %prog [options] [DIRS]
         %prog gives a graphic representation of the disk space
         usage of the folder trees under DIRS.""",
-        version='%prog 2.0.0')
+        version='%prog 2.0.1')
     cliparser.add_option(
         '-w', '--width',
         action='store', type='int', dest='display_width', default=terminal_width,
