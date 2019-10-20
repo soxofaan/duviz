f97f7edecb26062deaa28f911d6694e16bb0928b
Stefaan Lippens <soxofaan@gmail.com>
2019-10-20 12:12:22 +0200

    bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='duviz',
-    version='1.1.0',
+    version='1.1.1',
     description='Command line disk space visualization tool',
     long_description=long_description,
     url='https://github.com/soxofaan/duviz',
